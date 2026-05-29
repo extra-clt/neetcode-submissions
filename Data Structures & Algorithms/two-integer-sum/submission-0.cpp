c439a9dfd10b3a801ba0abe89ea343794e390279
extra-clt <muhammed_b230431ec@nitc.ac.in>
2026-05-30 01:08:23 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> um;
+        for (int i = 0; i < nums.size(); i++) {
+            if (um.count(target-nums[i])) return {um[target-nums[i]],i};
+            um[nums[i]] = i;
+        }
+        return {};
+    }
+};
