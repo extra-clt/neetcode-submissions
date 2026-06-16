c03a2081ef59b528a801a2d0b92328384570e4ae
extra-clt <muhammed_b230431ec@nitc.ac.in>
2026-06-16 07:34:02 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            nums[abs(nums[i])-1] *= -1;
+            if (nums[abs(nums[i])-1] > 0) return abs(nums[i]);
+        } 
+        return 0;
+    }
+    // 1 3 2 2 4
+};
