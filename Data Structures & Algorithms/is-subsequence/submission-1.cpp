8bf16e272144958036c060b4ac1c36d44ed05cd8
extra-clt <muhammed_b230431ec@nitc.ac.in>
2026-07-23 23:51:17 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (t.size() < s.size()) return false;
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) i++;
+            j++;
+        }
+        return i == s.size();
+    }
+};
