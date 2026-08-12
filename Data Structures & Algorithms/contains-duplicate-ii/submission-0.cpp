be6b4942d079dcd9796ee528da03cba0e8b02d87
extra-clt <muhammed_b230431ec@nitc.ac.in>
2026-08-12 12:36:07 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> um;
+        for (int i = 0; i < nums.size(); i++) {
+            if (um.count(nums[i]) && abs(um[nums[i]]-i) <= k) return true;
+            um[nums[i]] = i;
+        }
+        return false;
+    }
+};
